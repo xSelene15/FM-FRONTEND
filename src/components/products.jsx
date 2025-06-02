c6fdf8ad9f4ce85d7dd7595bdcc598acf2e845dd
Evelyn Lopez <evel.lopez@duocuc.cl>
2025-06-02 12:29:02 -0400

    Modificacion de cards en productos

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -1,35 +1,36 @@
 import React from 'react';
 import Card from '@mui/material/Card';
-import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
+import { CardContent, CardActions, Button} from '@mui/material';
 
 
 export default function IntroDivider({ nombre, marca, precioActual, descripcion, stock }) {
     return (
-        <Card variant="outlined" sx={{ maxWidth: 360 }}>
-            <Box sx={{ p: 2 }}>
-                <Stack
-                    direction="row"
-                    sx={{ justifyContent: 'space-between', alignItems: 'center' }}
-                >
-                    <Typography gutterBottom variant="h5" component="div">
-                        {nombre}
-                    </Typography>
-                    <Typography variant="subtitle1" color="text.secondary">
-                        {marca}
-                    </Typography>
-                    <Typography gutterBottom variant="h6" component="div">
-                        ${precioActual}
-                    </Typography>
-                </Stack>
+        <Card sx={{ maxWidth: 345 }}>
+            <CardContent sx={{ p: 2 }}>
+                <Typography gutterBottom variant='h4' component="div">}
+                    {}
+                </Typography>
+                <Typography gutterBottom variant="h5" component="div">
+                    {nombre}
+                </Typography>
+                <Typography variant="subtitle1" color="text.secondary">
+                    {marca}
+                </Typography>
+                <Typography gutterBottom variant="h6" component="div">
+                    ${precioActual}
+                </Typography>
                 <Typography variant="body2" sx={{ color: 'text.secondary' }}>
                     {descripcion}
                 </Typography>
                 <Typography variant="body2" sx={{ color: 'text.secondary', mt: 1 }}>
                     Stock: {stock}
                 </Typography>
-            </Box>
-            {/* ...rest of your card... */}
+            </CardContent>
+            <CardActions>
+                <Button size="small">Share</Button>
+                <Button size="small">Learn More</Button>
+            </CardActions>
         </Card>
     );
 }
